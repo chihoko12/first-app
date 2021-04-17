d9762a095965368872c194301e257a9128817d20
cu <harrierraum@hotmail.com>
2021-04-17 12:03:45 +0200

    make sidebar

---
+++ src/pages/Sidebar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="side">
+        <h2>Sidebar</h2>
+        <h5>Something in mind</h5>
+        <div className="fakeimg" style={{ height: 200}}>Image</div>
+        <p>blah blah blah</p>
+        <h3>Say something</h3>
+        <p>Lorem ipsum dolor sit</p>
+        <div className="fakeimg" style={{ height: 60}}>Image</div><br />
+        <div className="fakeimg" style={{ height: 60}}>Image</div><br />
+        <div className="fakeimg" style={{ height: 60}}>Image</div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar
