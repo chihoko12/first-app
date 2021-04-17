20bef13b712227af87a574c9e6a2fa0ea810cbb9
cu <harrierraum@hotmail.com>
2021-04-17 12:07:50 +0200

    make content

---
+++ src/pages/Content.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="main">
+        <h2>Lorem ipsum</h2>
+        <h5>reuuih ioierkjh kjhsdfkjh Dec 10, 2018</h5>
+        <div className="fakeimg" style={{ height: 200 }}>Image</div>
+        <p>Nisi vitae..</p>
+        <p>Serlkjkljlk  slkjfkjl serlkj</p>
+        <br />
+        <h2>Placerat vsljkdslkj</h2>
+        <h5>sdlkfj inter sflkj Sep21, 2018</h5>
+        <div className="fakeimg" style={{ height: 200}}>Image</div>
+        <p>Bidendum sdlkj lkj</p>
+        <p>slkjfdlkj slkjslkj slkjfdlkj</p>
+      </div>
+    );
+  }
+}
