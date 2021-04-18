0fb5057c4b69bb5199ee8672faab0cc8f0e26612
cu <harrierraum@hotmail.com>
2021-04-18 21:26:52 +0200

    delete unnecessary lines

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import About  from './About';
-import Contact from './Contact';
 
 ReactDOM.render(<App />, document.getElementById('root'));
